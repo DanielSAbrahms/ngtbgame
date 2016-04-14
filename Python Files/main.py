4843cdb92f091a49c583869326b0465692690447
Daniel <dabrahms@zagmail.gonzaga.edu>
2016-04-14 15:05:53 -0700

    Some Player images have 16 Bit counterparts

--- Python Files/main.py
+++ Python Files/main.py
@@ -214,8 +214,8 @@ UP = 'up'
 LEFT = 'left'
 RIGHT = 'right'
 # Stretches player image to fit realism
-expandLeftRight = (100, 105)
-expandUpDown = (90, 100)
+expandLeftRight = (96, 150)
+expandUpDown = (96, 150)
 playerBounds = terrainSprite(0)
 # </editor-fold>
 
